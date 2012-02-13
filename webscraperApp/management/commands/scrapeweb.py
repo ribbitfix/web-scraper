b618b916c3acbb78c59df9ff90c75305b1d21aa7
Rachel <rachelsakry@gmail.com>
2012-02-13 15:49:01 -0800

    models reorganized, scrapeweb modified, new db

--- webscraperApp/management/commands/scrapeweb.py
+++ webscraperApp/management/commands/scrapeweb.py
@@ -12,17 +12,28 @@ class Command(NoArgsCommand):
 		for t in jQuery('#forum_table td.col_f_content h4 a'):
 			topics.append(jQuery(t).attr('href'))
 
-		# for each topic, get topic title and posted links 
+		# for each topic, create Topic object and associated Link objects 
 		for topic in topics:
 			jQuery = PyQuery(url=topic)
 			title = jQuery('h1.ipsType_pagetitle').html()
 			topicLink = topic
-			t = Topic(title=title, topicLink=topicLink)
-			t.save()
+			t = Topic.objects.create(title=title, topicLink=topicLink)
 			for link in jQuery('div.entry-content a'):
 				url = jQuery(link).attr('href')
-				l = Link(topic=t, link=url)
-				try:				
-					l.save()
-				except IntegrityError:
-					pass
+				try:
+					l = Link.objects.get(link=url)
+				except Link.DoesNotExist:
+					l = Link.objects.create(link=url)
+				#l, created = Link.objects.get_or_create(link=url)
+				#t.links.add(l)
+
+				'''(This works, but the above is maybe better)				
+				if Link.objects.filter(link=url).exists():
+					l = Link.objects.filter(link=url)[0]
+					l.topic.add(t)
+				else:
+					l = Link(topic=t, link=link)
+					l.add()'''
+
+
+				
--- webscraperApp/models.py
+++ webscraperApp/models.py
@@ -3,12 +3,11 @@ from django.db import models
 class Topic(models.Model):
 	title = models.CharField(max_length=200)
 	topicLink = models.URLField()
-
+	links = models.ManyToManyField("Link")
 	def __unicode__(self):
 		return self.title
 
 class Link(models.Model):
-	topic = models.ForeignKey(Topic)
 	link = models.URLField(unique=True)
 
 	def __unicode__(self):
